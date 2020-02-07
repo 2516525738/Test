10fd1ed09049c3d55528dd29b14bad6245c1a9c7
cai <cai@DESKTOP-3KNSTU7>
2020-02-07 17:20:39 +0800

    更新

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -1,7 +1,10 @@
 package test;
 
 public class test {
-//3333
-	//2222
-	///4444
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+	//1111111111111111111111111111111111111111111	
+	}
+
 }
--- test/src/test/test2.java
+++ test/src/test/test2.java
@@ -1,7 +1,9 @@
 package test;
 
 public class test2 {
-//333
-	//222
-	//444
+public static void main(String[] args) {
+	System.out.println(2);
+	//2222222222222222222222222222222222222
+	//333333333333333333333333333333333333
+}
 }
